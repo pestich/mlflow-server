b1e06aa3f9d072ffa7d47cc2fb540b2648aed140
Anatoliy Pestich <a.a.pestich@gmail.com>
2023-11-18 13:46:36 +0300

    Update run_example.py

--- run_example.py
+++ run_example.py
@@ -20,10 +20,12 @@ X_train, X_test, y_train, y_test = train_test_split(df.data, df.target)
 mlflow.set_tracking_uri(os.getenv("MLFLOW_TRACKING_URI"))
 
 # Установка имени эксперимента в MLflow
-mlflow.set_experiment('НАЗВАНИЕ ВАШЕГО ПРОЕКТА')
+mlflow.set_experiment('Example Project')
 
 # # Включение автоматического логирования параметров и метрик MLflow
 mlflow.autolog()
+
+print('Starting...')
 
 # Создание нового run в рамках эксперимента в MLflow
 with mlflow.start_run():
@@ -32,3 +34,6 @@ with mlflow.start_run():
     prediction = rf.predict(X_test)
     # Явное логирование метрик
     mlflow.log_metric("my_metric", 0) 
+
+print('The experiment was successfully completed')
+
