04944e02dc12cdf0d68d43d7456adb472988da6f
Anatoliy Pestich <a.a.pestich@gmail.com>
2023-11-18 14:28:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,10 +10,7 @@ commands_to_execute = [
     "sudo sh get-docker.sh",
     "sudo docker compose up -d --build", 
     "sudo apt install python3-pip",
-    "sudo apt-get install python3-venv",
-    "python3 -m venv venv",
-    "source venv/bin/activate",
-    "pip3 install boto3 python-dotenv mlflow scikit-learn",
+    "pip3 install --no-warn-script-location boto3 python-dotenv mlflow scikit-learn",
     "python3 create_bucket.py"
 ]
 
